33039f30dfededd92a45ab63a64224a50cad7b2a
ju5t <ju5t@users.noreply.github.com>
2021-08-17 12:18:47 +0200

    chore: rename to directadmin-client

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='directadmin',
+setup(name='directadmin-client',
       version='0.0.1',
       description='A Python interface to the DirectAdmin API',
       url='https://github.com/sensson/python-directadmin',
